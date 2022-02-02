316e418ed40a14d85ba16599e56f897c238c4887
Prasad <Tadasadpra555@yahoo.com>
2022-02-02 23:32:30 +0530

    Committed the changes

---
+++ demo/src/demoNew/Gitwork.java
@@ -0,0 +1,12 @@
+package demoNew;
+
+public class Gitwork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to git");
+
+	}
+
+}
